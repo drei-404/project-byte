af2310f2606d0288e6759e7e977fca8035ef8a5d
drei-404 <andrei.ruiz.global@gmail.com>
2025-12-15 16:55:43 +0800

    feat: add title and sub content to hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,8 +1,9 @@
 export default function Hero() {
   return (
     <>
-      <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center bg-background border">
-        <h1 className="text-4xl">hello World</h1>
+      <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center bg-background px-8 py-20 border">
+        <h1 className="text-4xl font-extrabold text-[#154091]">Building Youth Through Technology Empowerment Project</h1>
+        <p className="my-4">is a multi-sectoral collaboration that seeks to empower the youth through technology training. The partnership includes government and non-government organizations in Cebu that are at the forefront of technology development and community building. The training center is located at DOST7 Complex, Sudlon, Lahug, Cebu City.</p>
       </div>
     </>
   );
