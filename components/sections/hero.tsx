617fe600106906dbbdc87ff016c35c5ad933124e
drei-404 <andrei.ruiz.global@gmail.com>
2025-12-15 16:49:01 +0800

    feat: add custom font size for tailwind

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <>
       <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center bg-background border">
-        <h1>hello World</h1>
+        <h1 className="text-4xl">hello World</h1>
       </div>
     </>
   );
