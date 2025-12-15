f765e401911f4188d9239f723c4f6d32f192e52c
drei-404 <andrei.ruiz.global@gmail.com>
2025-12-15 16:29:19 +0800

    feat: add hero section to main page

---
+++ components/sections/hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <>
+      <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center bg-background border">
+        <h1>hello World</h1>
+      </div>
+    </>
+  );
+}
