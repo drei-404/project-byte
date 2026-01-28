f2c5dff9f83d4938a2d745fd96affb3ac8e40781
drei-404 <andrei.ruiz.global@gmail.com>
2026-01-28 11:50:43 +0800

    feat: add project list

--- app/(public)/courses/partials/project-card.tsx
+++ app/(public)/courses/partials/project-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardAction,
@@ -10,25 +10,47 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+
+const projectItems = [
+  {
+    image: "/images/courses-and-projects/projects/wordpress-development.webp",
+    title: "WordPress Development",
+    tags: ["WordPress", "Elementor"],
+  },
+];
 
 export function ProjectCard() {
   return (
-    <Card className="relative mx-auto w-full max-w-sm pt-0 rounded-xl overflow-hidden">
-      <div className="absolute inset-0 z-30 aspect-video" />
-      <img
-        src="/images/no-image-found.webp"
-        alt="Event cover"
-        className="relative z-20 aspect-video w-full object-cover"
-      />
-      <CardContent className="">
-        <CardAction>
-        </CardAction>
-        <h3 className="text-foreground">WordPress Website Design</h3>
-        <div className="flex justify-end mt-4">
-          <Badge variant="outline"><span className="font-normal">Linux</span></Badge>
-        </div>
-      </CardContent>
-    </Card>
-  )
+    <>
+      {projectItems.map((item, index) => {
+        return (
+          <Card
+            key={index}
+            className="relative mx-auto w-full max-w-sm pt-0 rounded-xl overflow-hidden"
+          >
+            <div className="absolute inset-0 z-30 aspect-video" />
+            <img
+              src={item.image}
+              alt="Project"
+              className="relative z-20 aspect-video w-full object-cover"
+            />
+            <CardContent className="">
+              <CardAction></CardAction>
+              <h3 className="text-foreground">{item.title}</h3>
+              <div className="flex justify-end mt-4">
+                {item.tags.map((tag, tagIndex) => {
+                  return (
+                    <Badge key={tagIndex} variant="outline">
+                      <span className="font-normal">{tag}</span>
+                    </Badge>
+                  );
+                })}
+              </div>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </>
+  );
 }
