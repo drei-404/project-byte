5ab3137447e41a54eb684ee5e2bba77a273ffb2c
drei-404 <andrei.ruiz.global@gmail.com>
2026-01-30 15:12:39 +0800

    feat: and data table and render data from database

---
+++ app/(admin-dashboard)/organizations/columns.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Badge } from "@/components/ui/badge";
+import { IconCircleCheckFilled, IconLoader } from "@tabler/icons-react";
+import Link from "next/link";
+
+export type OrganizationsRow = {
+  id: string;
+  name: string
+  location: boolean
+  trainingStartedAt: string
+};
+
+export const columns: ColumnDef<OrganizationsRow>[] = [
+  {
+    accessorKey: "name",
+    header: () => <div className="text-center">Organization Name</div>,
+    cell: ({ getValue }) => (
+      <div className="text-center">{getValue<string>()}</div>
+    ),
+  },
+
+  {
+    accessorKey: "location",
+    header: () => <div className="text-center">Location</div>,
+    cell: ({ getValue }) => (
+      <div className="text-center">{getValue<string>()}</div>
+    ),
+  },
+
+  {
+    accessorKey: "trainingStartedAt",
+    header: () => <div className="text-center">Joined</div>,
+    cell: ({ getValue }) => (
+      <div className="text-center">{getValue<string>()}</div>
+    ),
+  },
+
+  {
+    id: "actions",
+    size: 60,
+    cell: ({ row }) => {
+      const newsId = row.original.id;
+
+      return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <Link href={`/news-post/update-news/${newsId}`} passHref>
+          <DropdownMenuItem asChild><span>Update</span></DropdownMenuItem>
+          </Link>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )},
+  },
+];
--- app/(admin-dashboard)/organizations/page.tsx
+++ app/(admin-dashboard)/organizations/page.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import prisma from "@/lib/db";
 
-export default function Organizations() {
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+type OrganizationsRow = {
+  name: string
+  location: boolean
+  trainingStartedAt: string
+}
+
+export default async function Organizations({ params }: PageProps) {
+
+  const org = await prisma.organization.findMany({
+    orderBy: { trainingStartedAt: "desc" },
+  })
+
+  const data: OrganizationsRow[] = org.map(org => ({
+    id: org.id,
+    name: org.name,
+    location: org.location,
+    trainingStartedAt: org.trainingStartedAt.toISOString(),
+  }))
+
+
+
   return (
-    <div>Organizations</div>
-  )
+    <>
+
+      <DataTable columns={columns} data={data} />
+    </>
+  );
 }
